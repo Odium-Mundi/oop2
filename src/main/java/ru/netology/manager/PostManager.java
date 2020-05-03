dcceb9b841326d8304e46e66d42755e3ade7eb57
Odium-Mundi <61995344+Odium-Mundi@users.noreply.github.com>
2020-05-03 16:48:37 +0300

    Update PostManager.java

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,7 +5,6 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
     
-
     public Post[] search(int ownerId, String domain,String query,boolean ownersOnly,int count,int offset) {
         //TODO:
         return null;
@@ -14,8 +13,4 @@ public class PostManager {
     public void delete(int ownerId, int postIid) {
         //TODO:
     }
-
-
-
-
 }
